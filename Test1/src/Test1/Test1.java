aee3f91d0afb4ef43ef8d297339ba7bf09b33745
Administrator <Administrator@PC-20200416IVWS>
2020-06-25 09:45:21 +0800

    2

--- Test1/src/Test1/Test1.java
+++ Test1/src/Test1/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("java");
 		System.out.println("≤‚ ‘");
+		System.out.println("v2v2v2");
 	}
 
 }
---
+++ Test1/src/Test1/Test2.java
@@ -0,0 +1,8 @@
+package Test1;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("test2");
+	}
+
+}
